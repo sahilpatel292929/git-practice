a1f8b678f9a33e15f13eea90d6ba4431db31adcd
sahil patel <sahilpatel.softvan@gmail.com>
2022-11-24 21:28:35 +0530

    First commit Feature

--- src/main/java/com/gitdemo/GitdemoApplication.java
+++ src/main/java/com/gitdemo/GitdemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitdemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Master");
+		System.out.println("Feature");
 		SpringApplication.run(GitdemoApplication.class, args);
 	}
 
